3802bf104200474f651451113078d70dc721b4cd
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2016-03-11 07:12:23 -0800

    basic wrapper to simplify usage and arguments

---
+++ index.js
@@ -0,0 +1,40 @@
+var Highlights = require('highlights');
+var highlighter = new Highlights();
+
+highlighter.requireGrammarsSync({
+  modulePath: require.resolve('language-handlebars/package.json')
+});
+
+highlighter.requireGrammarsSync({
+  modulePath: require.resolve('language-sass/package.json')
+});
+
+highlighter.requireGrammarsSync({
+  modulePath: require.resolve('language-sql/package.json')
+});
+
+var scopes = {
+  'hbs': 'text.html.handlebars',
+  'go': 'source.go',
+  'js': 'source.js',
+  'scss': 'source.css.scss',
+  'sql': 'source.sql'
+};
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+fileContents = '';
+
+process.stdin.on('data', function(chunk) {
+  fileContents += chunk.toString();
+});
+
+process.stdin.on('end', function() {
+  var html = highlighter.highlightSync({
+    fileContents: fileContents,
+    scopeName: scopes[process.argv[2]],
+  });
+
+  console.log(html);
+});
+
